b027d24fed8293a4606be91669cc96e6a02be768
Amir-FF <amir.computer.04.ir@gmail.com>
2026-08-23 20:50:09 +0330

    add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path")
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 
@@ -27,6 +28,10 @@ module.exports = {
         }),
 
         new CleanWebpackPlugin(),
+
+        new HtmlWebpackPlugin({
+            template: "index.html",
+        }),
         
     ],
 
