6ebb78998f413fc9eab25be93b9e739cfc3941ea
Amir-FF <amir.computer.04.ir@gmail.com>
2026-08-23 20:06:12 +0330

    add clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 
@@ -24,6 +25,8 @@ module.exports = {
         new MiniCssExtractPlugin ({
             filename: "CSS/style-[contenthash].css"
         }),
+
+        new CleanWebpackPlugin(),
         
     ],
 
