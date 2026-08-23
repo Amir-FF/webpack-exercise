61a249204ec0d42da758da15500fc80f9cd7c370
Amir-FF <amir.computer.04.ir@gmail.com>
2026-08-24 02:48:48 +0330

    add mode: development && production

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,9 +1,9 @@
 const path = require("path")
-const TerserPlugin = require('terser-webpack-plugin');
+// const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ESLintPlugin = require("eslint-webpack-plugin");
+// const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
 
@@ -15,12 +15,12 @@ module.exports = {
         assetModuleFilename: 'images/[name][ext]',
     },
 
-    optimization: {
-        minimize: true,
-        minimizer: [new TerserPlugin()],
-    },
+    // optimization: {
+    //     minimize: true,
+    //     minimizer: [new TerserPlugin()],
+    // },
 
-    mode: "none",
+    mode: "development",
 
     plugins: [
 
@@ -35,7 +35,7 @@ module.exports = {
             // publicPath: "./dist/",
         }),
 
-        new ESLintPlugin()
+        // new ESLintPlugin()
         
     ],
 
---
+++ webpack.prod.config.js
@@ -0,0 +1,81 @@
+const path = require("path")
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const ESLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = {
+
+    entry: "./src/app.js",
+
+    output: {
+        filename: "app-[contenthash].js",
+        path: path.resolve(__dirname, "./dist"),
+        assetModuleFilename: 'images/[name][ext]',
+    },
+
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
+
+    mode: "production",
+
+    plugins: [
+
+        new MiniCssExtractPlugin ({
+            filename: "CSS/style-[contenthash].css"
+        }),
+
+        new CleanWebpackPlugin(),
+
+        new HtmlWebpackPlugin({
+            template: "index.html",
+            // publicPath: "./dist/",
+        }),
+
+        // new ESLintPlugin()
+        
+    ],
+
+    module: {
+        rules: [
+
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+
+            {
+                test: /\.png/,
+                type: 'asset/resource',
+            },
+
+            {
+                test: /\.(eot|ttf|woff|woff2)$/i,
+                type: 'asset/inline',
+            },
+
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                    targets: "defaults",
+                    presets: [
+                        ['@babel/preset-env']
+                    ]
+                    }
+                }
+            },
+
+        ],
+    },
+}
